e4564ff292c368e3732e60748f2a02420c5d7d77
vladponcea <vladponcea@icloud.com>
2022-05-08 14:13:00 +0300

    cf

---
+++ codeforces/1186A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+typedef long long ll;
+ 
+int32_t main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    if(m >= n && k >= n)
+        cout << "YES";
+    else 
+        cout << "NO";
+}
--- codeforces/935A.cpp
+++ codeforces/935A.cpp
@@ -3,13 +3,18 @@
 using namespace std;
 typedef long long ll;
 
-int n;
+int n, ans;
 
 int main() {
     cin >> n;
-    for (int i = 0; i < n/2; i++) {
-        
+    for (int i = 1; i <= n/2; i++) {
+        int x = n-i;
+        if(!(x%i)) {
+            ans++;
+        }
     }
+
+    cout << ans;
 
     return 0;
 }
