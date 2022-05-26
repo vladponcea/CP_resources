2f99e45d839ade3dca6e2be5fa35099cf91f94af
vladponcea <vladponcea@icloud.com>
2022-05-26 19:40:17 +0300

    cf

--- codeforces/129/a.cpp
+++ codeforces/129/a.cpp
@@ -3,56 +3,37 @@
 using namespace std;
 typedef long long ll;
 
-int solve(int a[], int b[], int n, int m) {
-    int x = 0, ok = 0, c = a[0], i = 0, j = 1 ;
-    while(i < m && j < n) {
-        if(x) {
-            if(b[m-1] < c) {
-                return 1;
-            } else {
-                c = b[i++];
-            }
-            x = 0;
-        } else {
-            if(a[n-1] < c) {
-                return 1;
-            } else {
-                c = a[j++];
-            } 
-            x = 1;
-        } 
-    }
-    return 0;
-}
-
 int main() {
     int tt;
     cin >> tt;
     while(tt--) {
-        int n, m, a[51], b[51];
+        int n;
         cin >> n;
+        vector<int> a(n);
         for (int i = 0; i < n; i++) {
             cin >> a[i];
         }
-
+        
+        int m;
         cin >> m;
+        vector<int> b(m);
         for (int i = 0; i < m; i++) {
             cin >> b[i];
         }
-        sort(a, a + n);
-        sort(b, b + n);
-        
-        if(solve(a, b, n, m) == 1)
-            cout << "Alice" << '\n';
-        else 
-            cout << "Bob" << '\n';
-        
-        if(solve(b,  a, m, n) == 1) 
-            cout << "Bob" << '\n';
-        else 
-            cout << "Alice" << '\n';
 
+        int x = *max_element(a.begin(), a.end());
+        int y = *max_element(b.begin(), b.end());
+        if(x < y) {
+            cout << "Bob" << '\n';
+            cout << "Bob" << '\n';
+            continue;
+        }
+        if(x > y) {
+            cout << "Alice" << '\n';
+            cout << "Alice" << '\n';
+            continue;
+        }
+        cout << "Alice" << '\n';
+        cout << "Bob" << '\n';
     }
-
-    return 0;
 }
