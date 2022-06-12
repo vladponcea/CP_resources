9df42b649152560216aa2b88362ba11ac722858c
vladponcea <vladponcea@icloud.com>
2022-06-12 19:58:31 +0300

    leetcode biweekly contest 80 total fail

---
+++ leetcode/11062022/6095.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+    string s;
+    if(s.size() != 8) {
+        
+    }
+    for (int i = 0; i < s.size(); i++) {
+        if()
+    }
+
+    return 0;
+}
